fe650f4ec7ef0577dbca50b8a04b10c4c88821a7
Daniel Schneider <daschne8@gmail.com>
2019-06-13 16:46:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ class Route{
     this.endingLocation = endingLocation
   }
   blocksTravelled(){
-    let eastWest = ['1st Avenue','2nd Avenue','3rd Avenue','Lexington Avenue','Park','Madison Avenue','5th Avenue'];
+    const eastWest = ['1st Avenue','2nd Avenue','3rd Avenue','Lexington Avenue','Park','Madison Avenue','5th Avenue'];
     let blocksTravelled = 0
-    let horizontalTravelled = Math.abs(eastWest.indexOf(beginingLocation[0]) - eastWest.indexOf(endingLocation[0]) )
-    let verticalTravelled = Math.abs(beginingLocation[1] - endingLocation[1])
+    const horizontalTravelled = Math.abs(eastWest.indexOf(beginingLocation[0]) - eastWest.indexOf(endingLocation[0]) )
+    const verticalTravelled = Math.abs(beginingLocation[1] - endingLocation[1])
     return horizontalTravelled + verticalTravelled
   }
 }
